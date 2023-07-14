ec92b536ed66c1e56d23e0db68abb57419e6f0a8
Ijay Abby <inyangasi4@gmail.com>
2023-07-15 02:57:36 +0300

    Project structure

---
+++ src/components/DogDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Dogs</h1>
+    <ul>
+      <li v-for="dog in dogs" :key="dog.id">
+        <img :src="dog.url" alt="Dog">
+        <p>{{ dog.name }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    data() {
+      return {
+        dogs: []
+      };
+    },
+    mounted() {
+      this.getDogs();
+    },
+    methods: {
+      getDogs() {
+        axios.get("https://dog.ceo/api/breeds/list/all")
+          .then(response => {
+            this.dogs = response.data.message;
+            console.log()
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+</script>
+
+<style>
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+
+      li {
+        margin-bottom: 10px;
+      }
+
+      img {
+        width: 100px;
+      }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
 createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import VueLazyload from 'vue3-lazyload'
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(VueLazyload)
+  .mount('#app')
