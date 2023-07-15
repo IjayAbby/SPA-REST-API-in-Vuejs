79e76331b125678f8ed19ee4745b4fdb4c73f378
Ijay Abby <inyangasi4@gmail.com>
2023-07-15 06:48:12 +0300

    Dog details

--- src/components/DogDetail.vue
+++ src/components/DogDetail.vue
@@ -1,53 +1,62 @@
 <template>
-  <div>
-    <h1>Dogs</h1>
-    <ul>
-      <li v-for="dog in dogs" :key="dog.id">
-        <img :src="dog.url" alt="Dog">
-        <p>{{ dog.name }}</p>
-      </li>
-    </ul>
+  <div class="container my-4">
+    <div class="row">
+      <div class="col-12">
+        <h2>{{ dogDetails.name }}</h2>
+        <p><strong>Breed:</strong> {{ dogDetails.breed }}</p>
+        <p><strong>Size:</strong> {{ dogDetails.size }}</p>
+        <p><strong>Temperament:</strong> {{ dogDetails.temperament }}</p>
+      </div>
+      <div class="col-12">
+        <FilterButtons @apply-filter="applyFilter" />
+      </div>
+      <div class="col-12">
+        <img v-if="transformedImage" :src="transformedImage" class="img-fluid" alt="Transformed Dog">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import axios from "axios";
+import { mapGetters } from 'vuex'
+import FilterButtons from './FilterButtons.vue'
 
-  export default {
-    data() {
-      return {
-        dogs: []
-      };
-    },
-    mounted() {
-      this.getDogs();
-    },
-    methods: {
-      getDogs() {
-        axios.get("https://dog.ceo/api/breeds/list/all")
-          .then(response => {
-            this.dogs = response.data.message;
-            console.log()
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
+export default {
+  components: {
+    FilterButtons
+  },
+  computed: {
+    ...mapGetters(['getDogDetails', 'isLoading'])
+  },
+  data() {
+    return {
+      transformedImage: null
     }
-  };
+  },
+  methods: {
+    applyFilter(filterType) {
+      // Apply the selected filter to the dog image and update the transformedImage data
+      // using Cloudinary or a similar service
+    }
+  },
+  created() {
+    const breed = this.$route.params.breed
+    this.$store.dispatch('fetchDogDetails', breed)
+  }
+}
 </script>
 
 <style>
-      ul {
-        list-style: none;
-        padding: 0;
-      }
+.container {
+  margin-top: 2rem;
+}
 
-      li {
-        margin-bottom: 10px;
-      }
+.col-12:not(:last-child) {
+  margin-bottom: 1rem;
+}
 
-      img {
-        width: 100px;
-      }
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
 </style>
