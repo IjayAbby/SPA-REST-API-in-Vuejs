51985357641ba3e8b8638374abf3146278bcbe67
Ijay Abby <inyangasi4@gmail.com>
2023-07-18 07:08:09 +0300

    Code refactoer

--- src/components/DogList.vue
+++ src/components/DogList.vue
@@ -1,52 +1,59 @@
 <template>
     <div class="my-10">
-        <div class="flex flex-col items-center justify-center">
-             <h1 class="text-center mb-4 text-5xl font-semibold">Dog Images</h1>
-            <input type="text" id="search" placeholder="Search for a dog / breed" class="py-2 border-gray-500 rounded-[8px] placeholder:text-black focus:outline-orange-900 border-[1px] px-4 w-[50vw]"/>
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="text-center mb-4 text-5xl font-semibold">Dog Images</h1>
+        <select v-model="selectedBreed" @change="loadByBreed">
+          <option value="">Choose a dog breed</option>
+          <option v-for="breed in breeds" :key="breed" :value="breed">{{ breed }}</option>
+        </select>
+      </div>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 my-10 lg:my-20">
+        <div class="images" v-for="(image,index) in images" :key="index">
+          <router-link to="/dog/1">
+            <img v-lazy="{ src: image, loading: loadingImage, error: loadingImage }" :src="image"
+              class="md:h-72 w-full object-cover" alt="Dog">
+          </router-link>
         </div>
-        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 my-10 lg:my-20">
-            <div class="" v-for="(image,index) in dogImages" :key="index">
-                <router-link to="/dog/1">
-                    <img v-lazy="{ src: image, loading: loadingImage, error: loadingImage }" :src="image" class="md:h-72 w-full object-cover" alt="Dog">
-                </router-link>
-            </div>
-        </div>
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-import { mapGetters } from 'vuex';
-import DogService from '../services/DogService';
-import loadingImage from '../assets/loading.jpg'
-
-export default {
-    data(){
-        return {
-            dogImages:[],
-            loadingImage
-        };
+  <script>
+  import axios from 'axios';
+  import loadingImage from '../assets/loading.jpg'
+  
+  export default {
+    data() {
+      return {
+        breeds: [],
+        selectedBreed: '',
+        loadingImage,
+        images: []
+      };
     },
-    computed: {
-        ...mapGetters(['getDogImages']),
-    },
-    mounted() {
-        this.fetchDogImages();
+    async created() {
+      try {
+        const response = await axios.get("https://dog.ceo/api/breeds/list/all")
+        this.breeds = Object.keys(response.data.message)
+      } catch (e) {
+        console.log("There was a problem fetching the breed list.")
+      }
+      for (let i = 0; i < 100; i++) {
+        await this.loadRandomImage()
+      }
     },
     methods: {
-        async fetchDogImages() {
-            try {
-                // const breeds = await DogService.getBreeds();
-                // const randomBreed = Object.keys(breeds)[0]; // Fetch images for the first breed in the list
-                // const randomImage = await DogService.getRandomImageByBreed(randomBreed);
-                // const images = await DogService.getImagesByBreed(randomBreed);
-                // this.$store.commit('setDogImages', [...images, randomImage]);
-                const images = await DogService.getDogImages();
-                this.dogImages=images.data.message
-                console.log(images.data)
-            } catch (error) {
-                console.error('Error fetching dog images:', error);
-            }
-        },
+      async loadByBreed() {
+        if (this.selectedBreed) {
+          const response = await axios.get(`https://dog.ceo/api/breed/${this.selectedBreed}/images`)
+          this.images = response.data.message
+        }
+      },
+      async loadRandomImage() {
+        const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+        this.images.push(response.data.message)
+      }
     },
-};
-</script>
+  };
+  </script>
+  
