8e274e6f12874b216c47653fc120988977185e90
Ijay Abby <inyangasi4@gmail.com>
2023-07-23 07:39:43 +0300

    configure cloudinary to apply image transformation

--- src/components/FilterButtons.vue
+++ src/components/FilterButtons.vue
@@ -1,31 +1,60 @@
 <template>
-    <div class="container my-4">
-        <div class="row">
-            <div class="col-12">
-                <h3 class="mb-3">Image Filters</h3>
-                <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-outline-primary" @click="applyFilter('cartoonify')">
-                        Cartoonify
-                    </button>
-                    <button type="button" class="btn btn-outline-primary" @click="applyFilter('grayscale')">
-                        Grayscale
-                    </button>
-                    <button type="button" class="btn btn-outline-primary" @click="applyFilter('sepia')">
-                        Sepia
-                    </button>
-                </div>
-            </div>
-        </div>
+    <div>
+      <h2>Original Image</h2>
+      <AdvancedImage :cldImg="myImage" />
+  
+      <h2>Filter Previews</h2>
+      <div>
+        <button @click="applyFilter('sepia')">Sepia</button>
+        <button @click="applyFilter('grayscale')">Grayscale</button>
+        <button @click="applyFilter('cartoonify')">Cartoonify</button>
+      </div>
+  
+      <div v-for="filter in filters" :key="filter">
+        <h3>{{ filter }}</h3>
+        <AdvancedImage :cldImg="previewImages[filter]" />
+      </div>
     </div>
-</template>
+  </template>
 <script>
+
+import DogList from '../components/DogList.vue'
+import { AdvancedImage } from '@cloudinary/vue';
+import { Cloudinary } from "@cloudinary/url-gen";
+import { sepia, grayscale, cartoonify } from '@cloudinary/url-gen/actions/effect';
+
+
+//Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "ijayabby",
+  },
+});
+
+
 export default {
-    methods: {
-        applyFilter(filterType) {
-            // Emit an event with the selected filter type
-            this.$emit('apply-filter', filterType);
-        },
+  components: {
+    AdvancedImage,
+    DogList
+  },
+  data() {
+    const cld = new Cloudinary({ cloud: { cloudName: 'ijayabby' } });
+    const myImage = cld.image(`${publicId}`);
+    return {
+      myImage,
+      filters: ['sepia', 'grayscale', 'cartoonify'],
+      previewImages: {
+        sepia: myImage.clone().effect(sepia()),
+        grayscale: myImage.clone().effect(grayscale()),
+        cartoonify: myImage.clone().effect(cartoonify()),
+      },
+    };
+  },
+  methods: {
+    applyFilter(filter) {
+      this.myImage.effect(window[filter]());
     },
+  },
 };
 </script>
 
